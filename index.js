07e437d82bc1bcfedccc3b42e730dd026ab004d6
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2023-12-02 10:49:14 +0330

    finding habitable planets

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const fs  = require('fs');
 
 
 //Create an empty array to store the parsed CSV data
-const results = [];
+const habiTablePlanets = [];
 
+function isHabiTablePlanets(planet){
+ return planet['koi_disposition'] == 'CONFIRMED'
+ && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+ && planet['koi_prad'] <1.6;
+}
 
 //Set up a readable stream from the 'kepler_data.csv' file
 fs.createReadStream('kepler_data.csv')
@@ -16,7 +21,9 @@ fs.createReadStream('kepler_data.csv')
 
      //  Attach event listeners to the stream ,The data event provides chunks of data, and we are pushing these chunks directly into the results array.
         .on('data' , (data) =>{
-           results.push(data);
+           if(isHabiTablePlanets(data)){
+            habiTablePlanets.push(data);
+           }
         })
         //'error' event: Triggered in case of an error during reading the stream. It logs the error to the console.
         .on('error' , (err) => {
@@ -24,7 +31,7 @@ fs.createReadStream('kepler_data.csv')
         })
         //end event: Triggered when the stream has been fully read.
         .on('end' , ()=>{
-         console.log("Results",results);
-         console.log(' Streaming Done');
+         console.log(`${habiTablePlanets.length} habitable planets found!`);
+         console.log(' Done');
         })
  
